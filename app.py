56f07e1a3fdcf470d2c3e3bf6b1484ba28d773e3
romullossousa <romullossousa@gmail.com>
2025-03-02 19:35:38 +0000

    fix name folder

--- app.py
+++ app.py
@@ -203,6 +203,6 @@ def get_data():
     })
 
 # Executar aplicativo (apenas se rodar localmente; em produção usar gunicorn)
-if __name__ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
